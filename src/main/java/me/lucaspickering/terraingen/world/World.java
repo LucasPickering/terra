6db6565a42ffefb206b67451f11237403b0d4e0c
Lucas Pickering <lucaspickering9@gmail.com>
2016-12-31 00:00:35 -0500

    Added generation timer

--- src/main/java/me/lucaspickering/terraingen/world/World.java
+++ src/main/java/me/lucaspickering/terraingen/world/World.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 import java.util.EnumMap;
 import java.util.Map;
 import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import me.lucaspickering.terraingen.TerrainGen;
 import me.lucaspickering.terraingen.render.Renderer;
@@ -26,7 +28,7 @@ public class World {
     public static final InclusiveRange ELEVATION_RANGE = new InclusiveRange(-50, 75);
 
     // World size
-    private static final int SIZE = 20;
+    private static final int SIZE = 100;
 
     private static final Generator[] GENERATORS = new Generator[]{
         new BiomePainter(),
@@ -37,6 +39,7 @@ public class World {
         new BeachGenerator()
     };
 
+    private final Logger logger;
     private final Random random;
     private final Tiles tiles;
 
@@ -44,12 +47,15 @@ public class World {
     private Point worldCenter;
 
     public World() {
+        logger = Logger.getLogger(getClass().getName());
         random = TerrainGen.instance().random();
         tiles = genTiles();
         worldCenter = new Point(Renderer.RES_WIDTH / 2, Renderer.RES_HEIGHT / 2);
     }
 
     private Tiles genTiles() {
+        final long startTime = System.currentTimeMillis(); // We're timing this
+
         final Tiles tiles = new Tiles();
         // Fill out the set with a bunch of points
         for (int x = -SIZE; x <= SIZE; x++) {
@@ -81,7 +87,10 @@ public class World {
         // Apply each generator in sequence (this is the heavy lifting)
         Arrays.stream(GENERATORS).forEach(gen -> gen.generate(tiles, random));
 
-        return tiles.immutableCopy(); // Return an immutable copy of what we just made
+        final Tiles result = tiles.immutableCopy(); // Make an immutable copy
+        logger.log(Level.FINE, String.format("World generation took %d ms",
+                                             System.currentTimeMillis() - startTime));
+        return result;
     }
 
     /**
