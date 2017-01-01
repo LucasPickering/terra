ccf72efbc0a2119e17b2fadd99df979a18f9e907
Lucas Pickering <lucaspickering9@gmail.com>
2017-01-01 12:47:34 -0500

    Benchmark tweaks to make it faster

--- src/test/java/me/lucaspickering/terraingen/world/WorldBenchmark.java
+++ src/test/java/me/lucaspickering/terraingen/world/WorldBenchmark.java
@@ -2,6 +2,7 @@ package me.lucaspickering.terraingen.world;
 
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
 import org.openjdk.jmh.annotations.Measurement;
 import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.Param;
@@ -14,12 +15,13 @@ import java.util.concurrent.TimeUnit;
 
 @State(Scope.Benchmark)
 @Threads(1)
-@Warmup(iterations = 5, time = 1000, timeUnit = TimeUnit.MILLISECONDS)
+@Warmup(iterations = 3, time = 1000, timeUnit = TimeUnit.MILLISECONDS)
 @Measurement(iterations = 5, time = 1000, timeUnit = TimeUnit.MILLISECONDS)
+@Fork(value = 5)
 @BenchmarkMode(Mode.AverageTime)
 public class WorldBenchmark {
 
-    @Param({"100", "200"})
+    @Param({"100", "175"})
     private int size;
 
     @Benchmark
