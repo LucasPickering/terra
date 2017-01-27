ba65dc23ba460ff6849a904699473c23e2276878
Lucas Pickering <lucaspickering9@gmail.com>
2017-01-26 23:46:40 -0500

    New contract for generators

--- src/main/java/me/lucaspickering/terraingen/world/generate/Generator.java
+++ src/main/java/me/lucaspickering/terraingen/world/generate/Generator.java
@@ -3,14 +3,18 @@ package me.lucaspickering.terraingen.world.generate;
 import java.util.Map;
 import java.util.Random;
 
-import me.lucaspickering.terraingen.world.util.TilePoint;
-import me.lucaspickering.terraingen.world.World;
 import me.lucaspickering.terraingen.world.Tile;
+import me.lucaspickering.terraingen.world.World;
+import me.lucaspickering.terraingen.world.util.TilePoint;
 
 /**
  * Represents one stage in the world generation process. A {@link Generator} is given a
  * {@link Map} of {@link TilePoint}s to {@link Tile}s, representing a partially-generated
  * world. It modifies the tiles in that map, and returns the same map with all the same objects.
+ *
+ * Given the same {@link World} and {@link Random}, a {@link Generator} must make the exact same
+ * modification to the world in any two calls of its {@link #generate(World, Random)} function.
+ * In other words, it must be stateless.
  */
 @FunctionalInterface
 public interface Generator {
