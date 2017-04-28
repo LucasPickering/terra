e4558017db9f5f5758880f7d8b804fe412c8b0f4
Lucas Pickering <pickering.l@husky.neu.edu>
2017-04-28 14:58:39 -0400

    Added mappings for GLFW key inputs

--- src/main/java/me/lucaspickering/terra/input/InputHandler.java
+++ src/main/java/me/lucaspickering/terra/input/InputHandler.java
@@ -3,6 +3,7 @@ package me.lucaspickering.terra.input;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import me.lucaspickering.terra.Main;
@@ -20,11 +21,27 @@ public class InputHandler {
 
     public InputHandler(Main main) {
         this.main = main;
+        loadConfig();
     }
 
     private void loadConfig() {
         final Properties prop = Funcs.loadProperties(Constants.CFG_KEYS);
-        
+        logger.log(Level.INFO, "Loaded key config");
+
+        // Process the input from the properties class
+        for (String propName : prop.stringPropertyNames()) {
+            final String propValue = prop.getProperty(propName);
+            final KeyMapping keyMap = KeyMapping.getByName(propValue);
+            if (keyMap != null) {
+                logger.log(Level.FINE, String.format("Mapping key [%s] to action [%s]",
+                                                     keyMap, propName));
+                inputMapping.put(keyMap.getCode(), propName);
+
+            } else {
+                logger.log(Level.WARNING, String.format("Unknown key [%s] for action [%s]",
+                                                        propValue, propName));
+            }
+        }
     }
 
     public Point getMousePos() {
